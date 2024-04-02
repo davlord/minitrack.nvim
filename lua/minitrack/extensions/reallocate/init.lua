d70afa1fde470c735e316fed8fa56876c0c574eb
davlord <>
2024-04-02 23:47:44 +0200

    Reallocate is proportional

--- lua/minitrack/extensions/reallocate/init.lua
+++ lua/minitrack/extensions/reallocate/init.lua
@@ -6,6 +6,7 @@ local original_renderer = nil
 
 local function reallocate_details(day, map)
     local new_map = {}
+    local total_duration = 0;
     local duration_to_reallocate = 0;
     for _, report_line in ipairs(map) do
 	if report_line.topic == MinitrackConfig.topic_to_reallocate then
@@ -13,19 +14,18 @@ local function reallocate_details(day, map)
 	else
 	    table.insert(new_map, util.table.copy(report_line))
 	end
+	total_duration = total_duration + report_line.duration
     end
 
-    local duration_per_report = 0
-    local duration_per_report_excess = 0
-
-    if #new_map > 0 then
-	duration_per_report = duration_to_reallocate / #new_map
-	duration_per_report_excess = duration_to_reallocate % #new_map
-    end
+    local remaining_duration_to_reallocate = duration_to_reallocate
     for i, report_line in ipairs(new_map) do
-	report_line.duration = report_line.duration + duration_per_report
-	if i == 1 then
-	    report_line.duration = report_line.duration + duration_per_report_excess
+	if i == #new_map then
+	    report_line.duration = report_line.duration + remaining_duration_to_reallocate
+	    remaining_duration_to_reallocate = 0
+	else
+	    local duration_to_add = math.ceil(duration_to_reallocate * report_line.duration / total_duration + 0.5)
+	    report_line.duration = report_line.duration + duration_to_add
+	    remaining_duration_to_reallocate = remaining_duration_to_reallocate - duration_to_add
 	end
     end
 
